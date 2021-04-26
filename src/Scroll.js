610fc1f14b1912e33788b32986deb7d4e90ba4b4
Łukasz Oniszczuk <lukwin21@gmail.com>
2021-04-26 16:17:32 +0200

    My Application RoboFriends

---
+++ src/Scroll.js
@@ -0,0 +1,15 @@
+function Scroll(props) {
+  return (
+    <div
+      style={{
+        overflowY: "scroll",
+        border: "5px solid black",
+        height: "1000px",
+      }}
+    >
+      {props.children}
+    </div>
+  );
+}
+
+export default Scroll;
